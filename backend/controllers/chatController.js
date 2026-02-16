edd888adf2c9760baca3ed1c79ca33b580475eba
Relyful <13381270+Relyful@users.noreply.github.com>
2026-02-16 21:29:37 +0100

    Add openChatWithUser route

--- backend/controllers/chatController.js
+++ backend/controllers/chatController.js
@@ -1,8 +1,18 @@
 // const { prisma } = require("../lib/prisma.mjs");
-const chatServices = require('../services/chatServices');
+const chatServices = require("../services/chatServices");
 
 exports.getAllChat = async (req, res) => {
   const allChat = await chatServices.getAllChat();
   res.json(allChat);
 };
 
+exports.openChatWithUser = async (req, res) => {
+  const thisUser = req.user.id;
+  const userToChat = req.params.userId;
+  //Check if chat already exists
+  const existingChat = await chatServices.getChatWithUserId(thisUser, userToChat);
+  const chat = existingChat
+    ? existingChat
+    : await chatServices.createNewChatWithUser(thisUser, userToChat);
+  res.json(chat);
+};
--- backend/routers/chatRouter.js
+++ backend/routers/chatRouter.js
@@ -3,4 +3,7 @@ const chatController = require('../controllers/chatController');
 
 const chatRouter = new Router();
 
-chatRouter.get('/', chatController.getAllChat)
+chatRouter.get('/', chatController.getAllChat);
+chatRouter.get('/user/:userId', chatController.openChatWithUser);
+
+module.exports = chatRouter;
