506d7b836a1e2f187fedf2b8e058d5af4998f346
Relyful <13381270+Relyful@users.noreply.github.com>
2026-02-16 20:39:57 +0100

    Replace user id placeholder with user id from auth

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -10,32 +10,28 @@ exports.createUser = async (req, res) => {
 };
 
 exports.deleteUser = async (req, res) => {
-  //Add id from user auth
-  const currentUserId = null;
+  const currentUserId = req.user.id;
   const deletedUser = await userServices.deleteUser(currentUserId);
   res.json(deletedUser)
 };
 
 exports.updateProfilePic = async (req, res) => {
-  //add id from auth
   const picId = req.params.picId
-  const currentUserId = null;
+  const currentUserId = req.user.id;
   const udpatedUser = await userServices.updateProfilePic(currentUserId, picId);
   res.json(udpatedUser);
 };
 
 exports.updateDisplayName = async (req, res) => {
-  //get user id from auth
-  const userId = null;
+  const userId = req.user.id;
   const newDisplayName = req.params.displayName;
   const updatedUser = await userServices.updateDisplayName(userId, newDisplayName);
   res.json(updatedUser);
 }
 
 exports.updateAbout = async (req, res) => {
-  //get userid form auth
   const newAbout = req.data.aboutMe;
-  const userId = null;
+  const userId = req.user.id;
   const updatedUser = await userServices.updateAbout(userId, newAbout);
   res.json(updatedUser);
 }
