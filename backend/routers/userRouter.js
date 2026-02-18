0655bb0ace3014abb28063a067d7784abd18a111
Relyful <13381270+Relyful@users.noreply.github.com>
2026-02-18 21:30:46 +0100

    Change put to delete

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -4,7 +4,7 @@ const userController = require('../controllers/userController');
 const userRouter = Router();
 
 userRouter.post('/create', userController.createUser);
-userRouter.put('/delete', userController.deleteUser);
+userRouter.delete('/delete', userController.deleteUser);
 userRouter.put('/profilePic/:picId', userController.updateProfilePic);
 userRouter.put('/updateDisplayName/:displayName', userController.updateDisplayName);
 userRouter.put('/updateAbout', userController.updateAbout);
