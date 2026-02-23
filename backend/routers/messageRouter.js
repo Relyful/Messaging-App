69b1b21a83249e27cdddd730db91033779d5f738
Relyful <13381270+Relyful@users.noreply.github.com>
2026-02-23 21:38:44 +0100

    Add missing chatId param to new message route

--- backend/routers/messageRouter.js
+++ backend/routers/messageRouter.js
@@ -4,7 +4,7 @@ const authMidd = require('../middleware/authenticationMiddleware');
 
 const messageRouter = new Router();
 
-messageRouter.post('/new', authMidd, messageController.newMessage);
+messageRouter.post('/new:chatId', authMidd, messageController.newMessage);
 messageRouter.put('./delete/:messageId', authMidd, messageController.softDeleteMessage);
 
 module.exports = messageRouter;
