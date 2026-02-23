b4830c7142f938254b7baf73c4232104390cb47c
Relyful <13381270+Relyful@users.noreply.github.com>
2026-02-23 21:36:19 +0100

    Export router

--- backend/routers/messageRouter.js
+++ backend/routers/messageRouter.js
@@ -6,3 +6,5 @@ const messageRouter = new Router();
 
 messageRouter.post('/new', authMidd, messageController.newMessage);
 messageRouter.put('./delete/:messageId', authMidd, messageController.softDeleteMessage);
+
+module.exports = messageRouter;
