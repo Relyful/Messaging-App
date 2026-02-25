2b8cf89958c2ee85f77c033c767ef5feb8363687
Relyful <13381270+Relyful@users.noreply.github.com>
2026-02-25 20:21:52 +0100

    Split getting chat and creating chat to two

--- backend/controllers/chatController.js
+++ backend/controllers/chatController.js
@@ -6,14 +6,10 @@ exports.getAllChat = async (req, res) => {
   res.json(allChat);
 };
 
-exports.openChatWithUser = async (req, res) => {
+exports.createChatWithUser = async (req, res) => {
   const thisUser = req.user.id;
   const userToChat = req.params.userId;
-  //Check if chat already exists
-  const existingChat = await chatServices.getChatWithUserId(thisUser, userToChat);
-  const chat = existingChat
-    ? existingChat
-    : await chatServices.createNewChatWithUser(thisUser, userToChat);
+  const chat = await chatServices.createNewChatWithUser(thisUser, userToChat);
   res.json(chat);
 };
 
@@ -36,4 +32,11 @@ exports.updateChatName = async (req, res) => {
   const error = new Error("Chat not found");
   error.statusCode = 404;
   throw(error);
+}
+
+exports.getChatWithUserId = async (req, res) => {
+  const thisUser = req.user.id;
+  const userToChat = req.params.userId;
+  const chat = await chatServices.getChatWithUserId(thisUser, userToChat);
+  res.json(chat);
 }
