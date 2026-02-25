cc8fc9c822555ba232957deadc836f3e07ba5d7d
Relyful <13381270+Relyful@users.noreply.github.com>
2026-02-25 20:22:32 +0100

    Update routes to follow restful

--- backend/routers/chatRouter.js
+++ backend/routers/chatRouter.js
@@ -5,7 +5,8 @@ const authMidd = require('../middleware/authenticationMiddleware');
 const chatRouter = new Router();
 
 chatRouter.get('/', chatController.getAllChat);
-chatRouter.post('/user/:userId', authMidd, chatController.openChatWithUser);
+chatRouter.get('/user/:userId', authMidd, chatController.getChatWithUserId);
+chatRouter.post('/user/:userId', authMidd, chatController.createChatWithUser);
 chatRouter.post('/delete/:chatId', authMidd, chatController.deleteChat);
 chatRouter.put('/displayName/:chatId', authMidd, chatController.updateChatName);
 
