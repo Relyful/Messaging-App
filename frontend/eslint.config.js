003085d557511a67b0f15b65823924df47e302f1
Relyful <13381270+Relyful@users.noreply.github.com>
2026-03-12 21:00:24 +0100

    Fix react not in scope eslint err

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,9 +1,25 @@
 import js from "@eslint/js";
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+export default [
+  js.configs.recommended,
   pluginReact.configs.flat.recommended,
-]);
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off",
+    },
+  },
+];
