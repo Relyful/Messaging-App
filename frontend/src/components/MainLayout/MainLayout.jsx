e3d47bfaab505649b2c17aa54cebf5acdb605c05
Relyful <13381270+Relyful@users.noreply.github.com>
2026-03-17 21:17:43 +0100

    Create useeffect to fetch current user to display in header

--- frontend/src/components/MainLayout/MainLayout.jsx
+++ frontend/src/components/MainLayout/MainLayout.jsx
@@ -2,17 +2,46 @@ import styles from "./MainLayout.module.css";
 import { Link } from "react-router";
 import { Outlet } from "react-router";
 import Footer from "../Footer/Footer";
+import { useEffect } from "react";
 
 function MainLayout() {
+  const controller = new AbortController();
+  const fetchUser = async () => {
+    try {
+      const response = await fetch("http://localhost:8080/user/me", {
+        credentials: "include",
+        signal: controller.signal,
+      });
+      if (!response.ok) {
+        if (response.status == '404') {
+          // TODO: Set user to undefined 
+          return console.log('User not logged in')
+        }
+        throw new Error("Auth failed");
+      }
+      // TODO: SET USER STATE
+      const data = await response.json();
+      console.log(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  });
+
   return (
     <>
       <header className={styles.header}>
         <div className={styles.leftHeader}>
-          <Link to='/' className={styles.headerLink}><h2>Rely&apos;s Chat</h2></Link>
+          <Link to="/" className={styles.headerLink}>
+            <h2>Rely&apos;s Chat</h2>
+          </Link>
         </div>
         <div className={styles.rightHeader}>
-          <Link to='/'>Home</Link>
-          <Link to='/chat'>Chat</Link>
+          <Link to="/">Home</Link>
+          <Link to="/chat">Chat</Link>
         </div>
       </header>
       <main className={styles.container}>
@@ -20,7 +49,7 @@ function MainLayout() {
       </main>
       <Footer />
     </>
-  )
+  );
 }
 
 export default MainLayout;
