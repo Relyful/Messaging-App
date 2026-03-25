dc534e2fd65cbd65cee47fd11084db8d6fed6725
Relyful <13381270+Relyful@users.noreply.github.com>
2026-03-25 21:52:32 +0100

    Finishi logging in logic

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,41 +1,81 @@
-import styles from './Login.module.css';
-import { useFormStatus } from 'react-dom';
+import styles from "./Login.module.css";
+import { useFormStatus } from "react-dom";
+import { useOutletContext } from "react-router";
+import { fetchUser } from "../../api/userApi";
 
 function SubmitButton() {
   const { pending } = useFormStatus();
   return (
-    <button type="submit" disabled={pending} className={`${styles.formSubmitButt}`}>
+    <button
+      type="submit"
+      disabled={pending}
+      className={`${styles.formSubmitButt}`}
+    >
       {pending ? "Loggin In..." : "Log In"}
     </button>
   );
 }
 
-async function onLogInSubmit(formData) {
-const logInData = { username: formData.get("username"), password: formData.get("password") };
-try {
-  const response = await fetch('http://localhost:8080/login', {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      credentials: "include",
-      body: JSON.stringify(logInData),
-    });
-    if (!response.ok) {
-      throw new Error("Error logging in");
-    }
-    console.log(response);
-  } catch (err) {
-    console.error(err);
-  }
-}
-
 export default function Login() {
+  const [setUser] = useOutletContext();
+  async function updateUser() {
+    try {
+      const userData = await fetchUser();
+      if (!userData) {
+        setUser(undefined);
+      } else {
+        setUser(userData);
+      }
+    } catch (error) {
+      if (error.name !== "AbortError") {
+        console.error("Failed to fetch user:", error);
+        setUser(undefined);
+      }
+    }
+  }
+
+  async function onLogInSubmit(formData) {
+    const logInData = {
+      username: formData.get("username"),
+      password: formData.get("password"),
+    };
+    try {
+      const response = await fetch("http://localhost:8080/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+        body: JSON.stringify(logInData),
+      });
+      if (response.status === 401) {
+        setUser(null);
+      }
+      if (!response.ok) {
+        throw new Error("Error logging in");
+      }
+      console.log(response);
+      await updateUser();
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
   return (
     <div className={styles.logInContainer}>
-      <form action={onLogInSubmit} method="post" className={styles.logInForm}>
-        <input type="text" name="username" id="username" placeholder='Username' />
-        <input type="password" name="password" id="password" placeholder='Password'/>
+      <form action={onLogInSubmit} className={styles.logInForm}>
+        <input
+          type="text"
+          name="username"
+          id="username"
+          placeholder="Username"
+        />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Password"
+        />
         <SubmitButton />
       </form>
     </div>
