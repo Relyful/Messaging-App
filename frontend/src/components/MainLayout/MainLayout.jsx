13bddecfb1e7524ebd190afea5c78025942597a4
Relyful <13381270+Relyful@users.noreply.github.com>
2026-03-26 21:20:27 +0100

    Add logout button and formatting

--- frontend/src/components/MainLayout/MainLayout.jsx
+++ frontend/src/components/MainLayout/MainLayout.jsx
@@ -8,18 +8,18 @@ import { fetchUser } from "../../api/userApi";
 function MainLayout() {
   const [user, setUser] = useState(null);
 
-useEffect(() => {
+  useEffect(() => {
     const controller = new AbortController();
     const getUser = async () => {
       try {
-        const userData = await fetchUser(controller);        
+        const userData = await fetchUser(controller);
         if (!userData) {
           setUser(undefined);
         } else {
           setUser(userData);
         }
       } catch (error) {
-        if (error.name !== 'AbortError') {
+        if (error.name !== "AbortError") {
           console.error("Failed to fetch user:", error);
           setUser(undefined);
         }
@@ -41,11 +41,18 @@ useEffect(() => {
         <div className={styles.rightHeader}>
           <Link to="/">Home</Link>
           <Link to="/chat">Chat</Link>
-          {user ? <div>{user.username}</div> : <Link to='/login'>Log In</Link>}
+          {user ? (
+            <>
+              <div className={styles.headerUsername}>{user.username}</div>
+              <button className={styles.logOutButt}>Log Out</button>
+            </>
+          ) : (
+            <Link to="/login">Log In</Link>
+          )}
         </div>
       </header>
       <main className={styles.container}>
-        <Outlet context={[setUser]}/>
+        <Outlet context={[setUser]} />
       </main>
       <Footer />
     </>
