5fd7d57400848c1eab8470619a5ba9b3e316b1e5
Relyful <13381270+Relyful@users.noreply.github.com>
2026-03-30 22:02:57 +0200

    Start work on chatRow

--- frontend/src/components/ChatWindow/ChatWindow.jsx
+++ frontend/src/components/ChatWindow/ChatWindow.jsx
@@ -1,4 +1,31 @@
+import { useEffect } from 'react';
 import styles from './ChatWindow.module.css'
+import { fetchMyChats } from '../../api/userApi';
+
+function ChatRow() {
+
+  const fetchChatHandler = async (controller) => {
+    try {
+      const chats = await fetchMyChats(controller);
+      return chats;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    const controller = new AbortController();
+    fetchChatHandler(controller);
+    return () => controller.abort();
+  },[])
+
+  return (
+    <div className={styles.chatRow}>
+      <div className={styles.chatName}></div>
+      <div className={styles.lastMessage}></div>
+    </div>
+  )
+}
 
 function ChatWindow() {
   return (
@@ -7,6 +34,7 @@ function ChatWindow() {
         <h3 className={styles.topRowHeading}>Chat</h3>
         <div>New Chat</div>
       </div>
+      <ChatRow />
     </div>
   )
 }
