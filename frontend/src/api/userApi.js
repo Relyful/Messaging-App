b30a7f56132bd9e9b7098980fbb86d18eb2d7f37
Relyful <13381270+Relyful@users.noreply.github.com>
2026-04-09 20:41:12 +0200

    Create fetchChat method and add formating

--- frontend/src/api/userApi.js
+++ frontend/src/api/userApi.js
@@ -1,53 +1,70 @@
 export const fetchUser = async (controller = null) => {
-   try {
-      const response = await fetch("http://localhost:8080/user/me", {
-        credentials: "include",
-        signal: controller?.signal,
-      });
-      if (!response.ok) {
-        if (response.status == "404") {
-         return false;
-        }
-        throw new Error("Auth failed");
+  try {
+    const response = await fetch("http://localhost:8080/user/me", {
+      credentials: "include",
+      signal: controller?.signal,
+    });
+    if (!response.ok) {
+      if (response.status == "404") {
+        return false;
       }
-      console.log(response)
-      const data = await response.json();
-      return data;
-    } catch (err) {
-      console.error(err);
+      throw new Error("Auth failed");
     }
-  };
+    console.log(response);
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
+};
 
-  export const logOut = async (controller = null) => {
-   try {
-      const response = await fetch("http://localhost:8080/logout", {
-        method: "POST",
-        credentials: "include",
-        signal: controller?.signal,
-      });
-      if (!response.ok) {
-        throw new Error("Logout failed");
-      }
-      return true;
-    } catch (err) {
-      console.error(err);
-      return false
+export const logOut = async (controller = null) => {
+  try {
+    const response = await fetch("http://localhost:8080/logout", {
+      method: "POST",
+      credentials: "include",
+      signal: controller?.signal,
+    });
+    if (!response.ok) {
+      throw new Error("Logout failed");
     }
-  };
+    return true;
+  } catch (err) {
+    console.error(err);
+    return false;
+  }
+};
 
-  export const fetchMyChats = async (controller = null) => {
-   try {
-      const response = await fetch("http://localhost:8080/chat/my", {
-        credentials: "include",
-        signal: controller?.signal,
-      });
-      if (!response.ok) {
-        throw new Error("Auth failed");
-      }      
-      const data = await response.json();
-      console.log(data);
-      return data;
-    } catch (err) {
-      console.error(err);
+export const fetchMyChats = async (controller = null) => {
+  try {
+    const response = await fetch("http://localhost:8080/chat/my", {
+      credentials: "include",
+      signal: controller?.signal,
+    });
+    if (!response.ok) {
+      throw new Error("Auth failed");
     }
-  };
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const fetchChat = async (chatId, controller = null) => {
+  try {
+    const response = await fetch(`http://localhost:8080/chat/${chatId}`, {
+      credentials: "include",
+      signal: controller?.signal,
+    });
+    if (!response.ok) {
+      throw new Error("Chat could not be loaded");
+    }
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
+};
