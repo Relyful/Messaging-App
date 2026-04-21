b482933779078c7a43e97f603f4217f3922823f3
Relyful <13381270+Relyful@users.noreply.github.com>
2026-04-21 21:33:26 +0200

    Start work on sendMessage method

--- frontend/src/api/userApi.js
+++ frontend/src/api/userApi.js
@@ -68,3 +68,22 @@ export const fetchChat = async (chatId, controller = null) => {
     console.error(err);
   }
 };
+
+export const sendMessage = async (chatId, newMessageContent) => {
+  try {
+    const response = await fetch(`http://localhost:8080/message/new/${chatId}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({'content': newMessageContent}),
+    });    
+    if (!response.ok) {
+      throw new Error("Error sending message");
+    }
+    console.log(response);
+  } catch (err) {
+    console.error(err);
+  }
+}
