d13b84365942d56d00580bdf6eb47532f0147611
Relyful <13381270+Relyful@users.noreply.github.com>
2026-05-04 20:05:51 +0200

    Add user registration handler

--- frontend/src/components/Register/Register.jsx
+++ frontend/src/components/Register/Register.jsx
@@ -1,19 +1,28 @@
+import { registerUser } from '../../api/userApi';
 import styles from './Register.module.css';
 
 export default function Register() {
+
+  async function registerSubmitHandler(formData) {
+    const registerData = {
+      'username': formData.get('username'),
+      'password': formData.get('password'),
+    };
+    const callServerRegister = await registerUser(registerData);
+    console.log(callServerRegister);    
+  }
+
   return (
     <div className={styles.registerFormContainer}>
       <h2 className={styles.registerHeading}>Register</h2>
-      <form action='' className={styles.registerForm}>
+      <form action={registerSubmitHandler} className={styles.registerForm}>
         <label htmlFor="username">Username</label>
         <input type="text" name="username" id="username" className={styles.registerInput} />
         <label htmlFor="password">Password</label>
         <input type="password" name="password" id="password" className={styles.registerInput} />
         <label htmlFor="repeatPassword">Repeat password</label>
         <input type="password" name="repeatPassword" id="repeastPassword" className={styles.registerInput} />
-        <label htmlFor="displayName">Display name</label>
-        <input type="text" name="displayName" id="displayName" className={styles.registerInput} />     
-        <button type="button">Submit</button>   
+        <button type="submit">Submit</button>   
       </form>
     </div>
   )
