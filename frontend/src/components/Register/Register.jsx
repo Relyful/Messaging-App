ff6cf6356f6767f53da03686a721715c0a8920ac
Relyful <13381270+Relyful@users.noreply.github.com>
2026-05-04 19:27:23 +0200

    Add classes to elements

--- frontend/src/components/Register/Register.jsx
+++ frontend/src/components/Register/Register.jsx
@@ -6,13 +6,13 @@ export default function Register() {
       <h2 className={styles.registerHeading}>Register</h2>
       <form action='' className={styles.registerForm}>
         <label htmlFor="username">Username</label>
-        <input type="text" name="username" id="username" />
+        <input type="text" name="username" id="username" className={styles.registerInput} />
         <label htmlFor="password">Password</label>
-        <input type="password" name="password" id="password" />
+        <input type="password" name="password" id="password" className={styles.registerInput} />
         <label htmlFor="repeatPassword">Repeat password</label>
-        <input type="password" name="repeatPassword" id="repeastPassword" />
+        <input type="password" name="repeatPassword" id="repeastPassword" className={styles.registerInput} />
         <label htmlFor="displayName">Display name</label>
-        <input type="text" name="displayName" id="displayName" />     
+        <input type="text" name="displayName" id="displayName" className={styles.registerInput} />     
         <button type="button">Submit</button>   
       </form>
     </div>
