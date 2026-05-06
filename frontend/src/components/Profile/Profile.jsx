bb0ad51dd9bf9b390365c7c3e207efac9baef97d
Relyful <13381270+Relyful@users.noreply.github.com>
2026-05-06 21:54:42 +0200

    Light styling and fetch users data with useeffect

--- frontend/src/components/Profile/Profile.jsx
+++ frontend/src/components/Profile/Profile.jsx
@@ -1,7 +1,20 @@
+import { useEffect } from 'react';
 import styles from './Profile.module.css';
+import { fetchUserData } from '../../api/userApi';
+import { useOutletContext } from 'react-router';
 
 export default function Profile() {
   // Fetch stuff from backend based on users id saved in user state
+  // Add userData state and keep data there.
+  const { user }  = useOutletContext();
+
+  useEffect(() => {
+    const controller = new AbortController();
+    if (user) {
+      fetchUserData(user.id, controller);    }
+    return () => controller.abort();
+  }, [user]);
+
   return (
     <div className={styles.profileContainer}>
       <h2>User profile</h2>
