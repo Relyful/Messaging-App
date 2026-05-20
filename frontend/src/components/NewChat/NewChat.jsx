64429583eae25412581a1a957d60c190d55b7876
Relyful <13381270+Relyful@users.noreply.github.com>
2026-05-20 21:59:45 +0200

    Fetch userData in useEffect

--- frontend/src/components/NewChat/NewChat.jsx
+++ frontend/src/components/NewChat/NewChat.jsx
@@ -1,14 +1,35 @@
+import { useEffect, useState } from 'react'
 import styles from './NewChat.module.css'
+import { getAllUsers } from '../../api/userApi';
+
+// function UserCards({ usersData }) {
+//   // const cards = usersData.filter((user) => {})
+//   // Map filtered array and create user cards
+// }
+
 
 export default function NewChat() {
+  const [users, setUsers] = useState(null);
+
+  async function handleFetchUsers() {
+    const users = await getAllUsers();
+    setUsers(users);
+    console.log(users);
+  }
+
+  useEffect(() => {
+    handleFetchUsers();
+  }, [])
+
   return (
     <div className={styles.newChatContainer}>
       <div className={styles.headerContainer}>
-
+        <h2>This is a header</h2>
       </div>
       <div className={styles.userPicker}>
         {/* Show all users available to chat */}
+        {`${users}`}
       </div>
     </div>
   )
-}
+};
