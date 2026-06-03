c3017ed56a99d2e5d69f34cbd63798793afa7027
Relyful <13381270+Relyful@users.noreply.github.com>
2026-06-03 21:17:37 +0200

    fix import

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import routes from './routes';
 import { createBrowserRouter } from "react-router";
-import { RouterProvider } from "react-router/dom";
+import { RouterProvider } from "react-router";
 
 const router = createBrowserRouter(routes);
 
