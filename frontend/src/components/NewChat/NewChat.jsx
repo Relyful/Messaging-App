d94fdde3e036e8124ba40adf17aebcbe7f11b066
Relyful <13381270+Relyful@users.noreply.github.com>
2026-06-15 22:07:29 +0200

    Remove selected user on second click

--- frontend/src/components/NewChat/NewChat.jsx
+++ frontend/src/components/NewChat/NewChat.jsx
@@ -1,11 +1,11 @@
-import { useEffect, useState } from 'react'
-import styles from './NewChat.module.css'
-import { getAllUsers } from '../../api/userApi';
-import { useOutletContext, useNavigate } from 'react-router';
-import { createNewChat, existingChatCheck } from '../../api/chatApi';
+import { useEffect, useState } from "react";
+import styles from "./NewChat.module.css";
+import { getAllUsers } from "../../api/userApi";
+import { useOutletContext, useNavigate } from "react-router";
+import { createNewChat, existingChatCheck } from "../../api/chatApi";
 
 function UserCards({ usersData, mode }) {
-  const {user} = useOutletContext();
+  const { user } = useOutletContext();
   const [chosenUsers, setChosenUsers] = useState([]);
   const navigate = useNavigate();
   const filteredUsers = usersData.filter((userD) => userD.id != user.id);
@@ -14,30 +14,39 @@ function UserCards({ usersData, mode }) {
     const check = await existingChatCheck(id);
     if (check) {
       return navigate(`/chat/${check.id}`);
-    };
+    }
     const newChat = await createNewChat(id);
     navigate(`/chat/${newChat.id}`);
   }
 
-  function newChatOnClickHandlerGroup(userId) {
-    console.log(userId);
-    setChosenUsers([...chosenUsers, userId]);
-    //remove choice on second click
+  function newGroupChatOnClickHandler(userId) {
+    if (chosenUsers.includes(userId)) {
+      const newArray = chosenUsers.filter((item) => item !== userId);
+      setChosenUsers(newArray);
+    } else {
+      setChosenUsers([...chosenUsers, userId]);
+    }
   }
 
   const cards = filteredUsers.map((user) => {
     return (
-      <div className={`${styles.userCard} ${chosenUsers.includes(user.id) ? styles.active : null}`} key={user.id} onClick={mode === "solo" ? () => newChatOnClickHandler(user.id) : () => newChatOnClickHandlerGroup(user.id)}>
+      <div
+        className={`${styles.userCard} ${chosenUsers.includes(user.id) ? styles.active : null}`}
+        key={user.id}
+        onClick={
+          mode === "solo"
+            ? () => newChatOnClickHandler(user.id)
+            : () => newGroupChatOnClickHandler(user.id)
+        }
+      >
         <div className={styles.name}>
           {user.displayName || user.username}
-          {`${mode}`}
         </div>
       </div>
-    )
-  }) 
+    );
+  });
   return cards;
 }
-
 
 export default function NewChat({ mode }) {
   const [users, setUsers] = useState(null);
@@ -49,7 +58,7 @@ export default function NewChat({ mode }) {
 
   useEffect(() => {
     handleFetchUsers();
-  }, [])
+  }, []);
 
   return (
     <div className={styles.newChatContainer}>
@@ -60,5 +69,5 @@ export default function NewChat({ mode }) {
         {users && <UserCards usersData={users} mode={mode} />}
       </div>
     </div>
-  )
-};
+  );
+}
