0daa28bb073a6852b2e84df512e60c2eed94bc73
Relyful <13381270+Relyful@users.noreply.github.com>
2026-06-19 21:18:12 +0200

    Add basic layout

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,24 +1,28 @@
-import { useOutletContext } from 'react-router';
+import { useOutletContext, Link } from 'react-router';
 import styles from './Home.module.css';
+
 
 export default function Home() {
   const {user} = useOutletContext();
   console.log(user);
 
   return (
-    <div className={styles.container}>
+    <div className={styles.homeContainer}>
       <div className={styles.heading}>
         <h2>Welcome to Rely&apos;s Chat</h2>
-      </div>
-      <div className={styles.callToAction}>
+        <div className={styles.callToAction}>
         {!user ? (
           <>
             <p>Register to continue!</p>
             <p>Already an user? Log in!</p>
           </>
         ) : (
-        <div>Continue</div>
+        <div><Link to={'/chat'}>Continue</Link></div>
         )}
+      </div>
+      </div>      
+      <div className={styles.logo}>
+        This is going to be a logo
       </div>
     </div>
   )
