938d199864f702131e6951c92572057a235c5c88
Relyful <13381270+Relyful@users.noreply.github.com>
2026-06-26 18:13:07 +0200

    Add user list component and basic styling + close button

--- frontend/src/components/Chat/ChatMembersModal.jsx
+++ frontend/src/components/Chat/ChatMembersModal.jsx
@@ -1,17 +1,23 @@
 import styles from './ChatMembersModal.module.css'
 
-export default function ChatMembersModal({ modalState, modalToggle, chatMembers }) {
-  console.log(chatMembers)  
+function ChatMemberList({ chatMembers }) {
+  const list = chatMembers.map(element => {
+          return <div className={styles.gCMemberRow} key={element.user.id}>{element.user.displayName ? element.user.displayName : element.user.username}</div>
+        })
+  return (
+    <div className={styles.memberListContainer}>
+      {list}
+    </div>
+  )
+}
 
+export default function ChatMembersModal({ modalState, modalToggle, chatMembers }) {
   return (
     <div className={`${styles.modalBackdrop} ${modalState ? styles.isOpen : null}`} onClick={modalToggle}>
       <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
-        <h3>Chat members</h3>
-        {/* User list goes here */}
-        {/* {chatMembers} */}
-        {chatMembers.map(element => {
-          return <div key={element.user.id}>{element.user.username}</div>
-        })}
+        <button className={styles.modalClose} onClick={modalToggle}>&times;</button>
+        <h3 className={styles.chatMembersHeading}>Chat members</h3>
+        <ChatMemberList chatMembers={chatMembers}/>
       </div>
     </div>
   )
