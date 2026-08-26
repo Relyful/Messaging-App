67a9ab4bf8c716256d643388d811289e7378f660
Relyful <13381270+Relyful@users.noreply.github.com>
2026-08-26 21:10:46 +0200

    Style registration form

--- frontend/src/components/Register/Register.jsx
+++ frontend/src/components/Register/Register.jsx
@@ -17,12 +17,12 @@ export default function Register() {
       <h2 className={styles.registerHeading}>Register</h2>
       <form action={registerSubmitHandler} className={styles.registerForm}>
         <label htmlFor="username">Username</label>
-        <input type="text" name="username" id="username" className={styles.registerInput} />
+        <input type="text" name="username" id="username" className={styles.registerInput} placeholder='Username' />
         <label htmlFor="password">Password</label>
-        <input type="password" name="password" id="password" className={styles.registerInput} />
+        <input type="password" name="password" id="password" className={styles.registerInput} placeholder='Password' />
         <label htmlFor="repeatPassword">Repeat password</label>
-        <input type="password" name="repeatPassword" id="repeastPassword" className={styles.registerInput} />
-        <button type="submit">Submit</button>   
+        <input type="password" name="repeatPassword" id="repeastPassword" className={styles.registerInput} placeholder='Repeat Password' />
+        <button className={styles.formSubmitButt} type="submit">Submit</button>   
       </form>
     </div>
   )
