953baf2a3bd110b5da2aea02b6118ac2559d661c
Relyful <13381270+Relyful@users.noreply.github.com>
2026-09-03 16:14:26 +0200

    Change post to better fitting delete

--- backend/routers/chatRouter.js
+++ backend/routers/chatRouter.js
@@ -7,7 +7,7 @@ const chatRouter = new Router();
 chatRouter.get('/', chatController.getAllChat);
 chatRouter.get('/user/:userId', authMidd, chatController.getChatWithUserId);
 chatRouter.post('/user/:userId', authMidd, chatController.createChatWithUser);
-chatRouter.post('/delete/:chatId', authMidd, chatController.deleteChat);
+chatRouter.delete('/delete/:chatId', authMidd, chatController.deleteChat);
 chatRouter.put('/displayName/:chatId', authMidd, chatController.updateChatName);
 chatRouter.get('/my', authMidd, chatController.getUsersChats);
 chatRouter.get('/:chatId', authMidd, chatController.getChatById);
