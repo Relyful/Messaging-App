0ec7b163b85c2d636fb17938aa8a653debbace38
Relyful <13381270+Relyful@users.noreply.github.com>
2026-09-04 20:00:07 +0200

    Create basic footer and styling

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import styles from './Footer.module.css';
 
 function Footer() {
   return (
-    <footer className={styles.footerContainer}>This is a footer</footer>
+    <footer className={styles.footerContainer}>© 2026 Relyful</footer>
   )
 };
 
