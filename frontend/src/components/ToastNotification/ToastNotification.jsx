f0cfe35d32b642797b8614759c05af449c2bb425
Relyful <13381270+Relyful@users.noreply.github.com>
2026-09-11 14:01:35 +0200

    Create toast notification component

---
+++ frontend/src/components/ToastNotification/ToastNotification.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+import styles from './ToastNotification.module.css';
+
+export default function ToastNotification({ message, type, onClose}) {
+  const [visible, setVisible] = useState(null);
+
+  useEffect(() => {
+    setVisible(true);
+
+    const fadeOutTimer = setTimeout(() => setVisible(false), 4500);
+    const autoCloseTimer = setTimeout(() => onClose(), 5000)
+
+    return () => {
+      clearTimeout(fadeOutTimer);
+      clearTimeout(autoCloseTimer);
+    }
+  }, [message]);
+
+  return (
+    <div className={styles.modalContainer}>
+      <div className={`${styles.modal} ${visible ? styles.open : ''} ${styles[type]}`}>
+        {message}
+      </div>
+    </div>
+  )
+}
