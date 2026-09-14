474d4303bebf384c234df1017246bec27537da6d
Relyful <13381270+Relyful@users.noreply.github.com>
2026-09-14 12:21:12 +0200

    Mobile layout

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,9 +1,8 @@
-import { useOutletContext, Link } from 'react-router';
-import styles from './Home.module.css';
-
+import { useOutletContext, Link } from "react-router";
+import styles from "./Home.module.css";
 
 export default function Home() {
-  const {user} = useOutletContext();
+  const { user } = useOutletContext();
   console.log(user);
 
   return (
@@ -11,19 +10,50 @@ export default function Home() {
       <div className={styles.heading}>
         <h2>Welcome to Rely&apos;s Chat</h2>
         <div className={styles.callToAction}>
-        {!user ? (
-          <>
-            <p><Link className={styles.actionLink} to={'/register'}>Register</Link> to continue!</p>
-            <p>Already an user? <Link className={styles.actionLink} to={'/login'}>Log in!</Link></p>
-          </>
-        ) : (
-        <div><Link className={styles.actionLink} to={'/chat'}>Continue</Link></div>
-        )}
+          {!user ? (
+            <>
+              <p>
+                <Link className={styles.actionLink} to={"/register"}>
+                  Register
+                </Link>{" "}
+                to continue!
+              </p>
+              <p>
+                Already an user?{" "}
+                <Link className={styles.actionLink} to={"/login"}>
+                  Log in!
+                </Link>
+              </p>
+            </>
+          ) : (
+            <div>
+              <Link className={styles.actionLink} to={"/chat"}>
+                Continue
+              </Link>
+            </div>
+          )}
+        </div>
       </div>
-      </div>      
       <div className={styles.logo}>
-        This is going to be a logo
+        <svg
+        className={styles.svgLogo}
+          width="120"
+          height="120"
+          viewBox="0 0 100 100"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M 28 80 V 20 H 58 C 72 20 72 48 58 48 H 28 M 46 48 L 72 80"
+            stroke="white"
+            strokeWidth="7"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <circle cx="28" cy="20" r="3.5" fill="white" />
+          <circle cx="72" cy="80" r="3.5" fill="white" />
+        </svg>
       </div>
     </div>
-  )
+  );
 }
